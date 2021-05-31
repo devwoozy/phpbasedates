ee019a609ba681ad67ba9297ac7222ca85d506b3
devwoozy <marcoperezD@outlook.com>
2021-05-31 17:45:40 -0400

    datos php

---
+++ aplicacion/index.php
@@ -0,0 +1,16 @@
+<!doctype html> <html lang="es"> <head> <!-- Required meta tags --> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/boostrap.min.css"> 
+    <title>nombredeltab</title>
+  </head>
+  <body>
+    <h1>titulo</h1>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="js/jquery-3.6.0.min.js" ></script>
+    <script src="js/bootstrap.min.js" ></script>
+  </body>
+</html>
